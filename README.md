4f1a42131f7e9c44e8072a4534de8888fb03b13d
Jake Foster <jake@alley.co>
2021-05-27 12:58:30 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# searchpress-attachments
-SearchPress Attachment File Contents
+# SearchPress Attachments Add-On
+
+The [ingest attachment plugin](https://www.elastic.co/guide/en/elasticsearch/plugins/current/ingest-attachment.html) lets Elasticsearch extract file attachments in common formats (such as PPT, XLS, and PDF) by using the Apache text extraction library Tika. The source field must be a base64 encoded binary.
+
+## Requirements
+
+* [SearchPress v0.4+](https://github.com/alleyinteractive/searchpress)
+* Elasticsearch v7+
+* [Elasticsearch's Ingest attachment plugin](https://www.elastic.co/guide/en/elasticsearch/plugins/current/ingest-attachment.html)
+
+## Instructions
+
+To use this plugin, you must first ensure that the ingest attachment plugin is active on the Elasticsearch node that your project is using. You can confirm which plugins are currently active on your node by sending a GET request to `<my-es-endpoint-url>/_cat/plugins`. Instructions for installing the plugin are unique to the hosting service, so be sure to confirm that the plugin can be used in advance of planning to use this add-on.
+
+Once the ingest attachment plugin is installed on your node, you must also ensure that SearchPress is also active. Once your project meets these two requireents, simply install and activate this add-on. No further configuration is necessary.
+
+You may find that your indexing operations fail if the index request attempts to send too much data. If this is the case, you will likely need to find a good balance between the value set for `sp_attachments_max_file_size` and SearchPress' bulk size `\SP_Sync_Meta()->bulk`. Lowering both of these will result in smaller remote request sizes sent to the ES instance, but the degree to which either (or both) are lowered will depend on project specifics.
+
+## Filters
+
+* `sp_attachments_max_file_size` - Filters the max file size for indexed attachments. If a file exceeds this limit, the file contents will not be added to the index for this document. The attachment data will otherwise be indexed. Defaults to 5MB.
+* `sp_attachments_index_file_contents` - Filters whether or not a given file's contents should be indexed. This can be used to override indexing file contents that would otherwise be skipped by `sp_attachments_max_file_size`.
+*
---
+++ tests/test-meta.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Searchpress Attachments Tests: Test_Meta Class
+ *
+ * @package Searchpress_Attachments
+ * @subpackage Tests
+ */
+
+/**
+ * Tests the functionality of the meta helpers.
+ */
+class Test_Meta extends WP_UnitTestCase {
+
+	/**
+	 * Tests the functionality of register_meta_helper.
+	 */
+	public function test_register_meta_helper() {
+
+		// Register post meta to test.
+		\Searchpress_Attachments\register_meta_helper(
+			'post',
+			[ 'post' ],
+			'test_post_meta_key'
+		);
+
+		// Register term meta to test.
+		\Searchpress_Attachments\register_meta_helper(
+			'term',
+			[ 'category' ],
+			'test_term_meta_key'
+		);
+
+		// Ensure meta is registered for the post type specified.
+		$registered = get_registered_meta_keys( 'post', 'post' );
+		$this->assertNotEmpty( $registered['test_post_meta_key'] );
+
+		// Ensure defaults were applied properly.
+		$this->assertArraySubset(
+			[
+				'show_in_rest' => true,
+				'single'       => true,
+				'type'         => 'string',
+			],
+			$registered['test_post_meta_key']
+		);
+
+		// Ensure meta is not registered for a different post type.
+		$registered = get_registered_meta_keys( 'post', 'page' );
+		$this->assertFalse( isset( $registered['test_post_meta_key'] ) );
+
+		// Ensure meta is registered for the term type specified.
+		$registered = get_registered_meta_keys( 'term', 'category' );
+		$this->assertNotEmpty( $registered['test_term_meta_key'] );
+
+		// Ensure defaults were applied properly.
+		$this->assertArraySubset(
+			[
+				'show_in_rest' => true,
+				'single'       => true,
+				'type'         => 'string',
+			],
+			$registered['test_term_meta_key']
+		);
+
+		// Ensure meta is not registered for a different term type.
+		$registered = get_registered_meta_keys( 'term', 'post_tag' );
+		$this->assertFalse( isset( $registered['test_term_meta_key'] ) );
+
+		// Ensure custom options are supported.
+		\Searchpress_Attachments\register_meta_helper(
+			'post',
+			[ 'post' ],
+			'test_custom_meta_key',
+			[
+				'sanitize_callback' => 'absint',
+				'show_in_rest'      => false,
+				'single'            => false,
+				'type'              => 'integer',
+			]
+		);
+		$registered = get_registered_meta_keys( 'post', 'post' );
+		$this->assertNotEmpty( $registered['test_custom_meta_key'] );
+		$this->assertArraySubset(
+			[
+				'sanitize_callback' => 'absint',
+				'show_in_rest'      => false,
+				'single'            => false,
+				'type'              => 'integer',
+			],
+			$registered['test_custom_meta_key']
+		);
+	}
+}
